1a5bd6b87d2ebf7d3ff52177ba1e12893a496167
Andrei Misca <miscaandrei@gmail.com>
2013-12-04 13:48:09 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import *
 import dbindexer
 from django.contrib import admin
+from app1.views import *
+
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
@@ -14,4 +16,13 @@ urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
     ('^admin/', include(admin.site.urls)),
+    url(r'^basic/$', basic),
+    url(r'^data/$', data_hora),
+    url(r'^clients/$', clients),
+    url(r'^contact/$', contact),
+    url(r'^thanks/$', thanks),
+    url(r'^comanda/$', comanda),
+    url(r'^productes/$', producte),
+
+
 )
