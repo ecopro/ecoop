288ef8965c171380b608824e602c3aafa3dc626a
Andrei Misca <miscaandrei@gmail.com>
2013-12-04 13:47:48 +0100

    Update views.py

--- app1/views.py
+++ app1/views.py
@@ -1,1 +1,48 @@
+from django.http import *
+import datetime
+from django.shortcuts import *
+from ayuda import *
+from app1.models import *
 # Create your views here.
+#return render_to_response('sometemplate.html')
+
+def basic(request):
+    return HttpResponse("Hello world")
+
+
+def data_hora(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def clients(request):
+    return render_to_response('clients.html')
+
+def thanks(request):
+    return render_to_response('thanks.html')
+
+
+def contact(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ContactForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = ContactForm() # An unbound form
+
+    return render(request, 'contact.html', {
+        'form': form,
+    })
+
+
+def comanda(request):
+    comandes = Comanda.objects.order_by('data_recollida_comanda')
+    context = {"comandes":comandes}
+    return render(request,'comandes.html',context)
+
+def producte(request):
+    productes = Producte.objects.order_by('nom_prod')
+    context = {"productes":productes}
+    return render(request,'producte.html',context)
