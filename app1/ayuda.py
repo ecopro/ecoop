7ab2be748a34ef4732b6d0f1b395bca439c06e29
Misca Andrei <miscaandrei@gmail.com>
2013-12-17 11:26:23 +0100

    Afegir l'opcio de seleccionar el producte i de data de recollida

--- app1/ayuda.py
+++ app1/ayuda.py
@@ -1,15 +1,33 @@
 from django import forms
 from django.db import models
 from app1.models import *
+import datetime 
 
 
-valor_clients=[]
-x=Client.objects.order_by('ref_client')
+def divendres_tancat(): #comprova si avui es diendres per saber si encara es pot fer la comanda per la proxima setmana o per d'aui 2 setmanes
+	#avui = datetime.datetime(2013, 12, 21) #nomes per simular que avui es dv
+	avui=datetime.date.today() #agafa el dia actual 
+	if avui.weekday()>4:
+		return True
+	else:
+		return False
 
-for c in x:
-	valor_clients.append((c.ref_client , c.nom_client))
+def dates_entrega():
+	divendres=divendres_tancat()
+	avui = datetime.date.today()
+	if divendres== True :
+		avui = avui + datetime.timedelta(days=-avui.weekday() + 1, weeks=2) #si avui es passat dv la comanda sera per d'aqui 2 setmanes
+	else:
+		avui = avui + datetime.timedelta(days=-avui.weekday() + 1, weeks=1) # si avui encara no es dv la comanda es pot fer per la proxima setmana
+	dates_entrega = []
+	dates_entrega.append(('1', str(avui)))
+	dates_entrega.append(('2', str(avui+datetime.timedelta(weeks=1))))
+	dates_entrega.append(('3', str(avui+datetime.timedelta(weeks=2))))
+	dates_entrega.append(('4', str(avui+datetime.timedelta(weeks=3))))
+	dates_entrega=tuple(dates_entrega)
+	return dates_entrega
 
-valor_clients=tuple(valor_clients)
+
 
 class ContactForm(forms.Form):
     subject = forms.CharField(max_length=100)
@@ -18,9 +36,21 @@ class ContactForm(forms.Form):
     cc_myself = forms.BooleanField(required=False)
 
 class ClientForm(forms.Form):
-    valor = forms.ChoiceField(choices=valor_clients)
+	valor_clients=[]
+	x=Client.objects.order_by('ref_client')
+	for c in x:
+		valor_clients.append((c.ref_client , c.nom_client))
+
+	valor_clients=tuple(valor_clients)
+	valor = forms.ChoiceField(choices=valor_clients)
 
 class FesComandaForm(forms.Form):
-	productes = forms.CharField()
-	data_entrega = forms.DateField() 
+	prod=[]
+	x=Producte.objects.order_by('ref_prod')
+	for c in x:
+		prod.append((c.ref_prod , c.nom_prod))
+	prod=tuple(prod)
+	productes = forms.ChoiceField(choices=prod)
+	quantitat=forms.IntegerField(max_value=12)
+	data_entrega = forms.ChoiceField(choices=dates_entrega())
 	#valor = forms.ChoiceField(choices=valor_comandes)
